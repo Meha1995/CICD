36858e4cafeec335589e655a95519d561b1a2e75
Meha Mahajan <memahajan@bebotechnologies.com>
2023-09-01 13:20:38 +0530

    console print

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,6 +3,7 @@ const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+  console.log("Navigated to the site")
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
